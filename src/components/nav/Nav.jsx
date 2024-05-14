27281fccbaa74433ea5a85a50dba86ac87d8a400
BaptLab <baptiste.labaune@gmail.com>
2024-05-14 16:36:41 +0200

    working on style

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,19 +1,48 @@
+import React, { useState } from "react";
 import NavBtn from "../btn/nav-btn/NavBtn";
 import "./nav.css";
 
 function Nav() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const switchNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
     <nav>
-      <div className="nav-container" id="backBtn-container">
-        <NavBtn txt="Retour"></NavBtn>
+      <div className="regular-nav">
+        <div
+          className="nav-container"
+          id="backBtn-container"
+        >
+          <NavBtn txt="Retour"></NavBtn>
+        </div>
+        <div className="nav-container">
+          <NavBtn txt="Accueil"></NavBtn>
+          <NavBtn txt="Tarifs"></NavBtn>
+          <NavBtn txt="Matériel"></NavBtn>
+          <NavBtn txt="Projets"></NavBtn>
+          <NavBtn txt="Me contacter"></NavBtn>
+        </div>
       </div>
-      <div className="nav-container">
+      <div
+        className={`sidenav ${isNavOpen ? "active" : ""}`}
+      >
         <NavBtn txt="Accueil"></NavBtn>
         <NavBtn txt="Tarifs"></NavBtn>
         <NavBtn txt="Matériel"></NavBtn>
         <NavBtn txt="Projets"></NavBtn>
         <NavBtn txt="Me contacter"></NavBtn>
       </div>
+
+      <a href="#" className="openBtn" onClick={switchNav}>
+        <span className="burger-icon">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+      </a>
     </nav>
   );
 }
