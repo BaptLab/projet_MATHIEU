886dc6456c31c4b420e9068375e6559bdfd7c1dc
BaptLab <baptiste.labaune@gmail.com>
2024-05-14 15:39:14 +0200

    working on style

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -8,6 +8,8 @@ import data from "../../datas/txt/homepage.json";
 import camera from "../../assets/images/icons/camera.svg";
 import headphones from "../../assets/images/icons/headphones.svg";
 import sound from "../../assets/images/icons/sound.svg";
+import studioImg from "../../assets/images/photos/studio/studio.jpg";
+import "./homepage.css";
 
 function Homepage() {
   const [tournage, mixage, montage] = data.activités;
@@ -15,34 +17,45 @@ function Homepage() {
   return (
     <div className="page-container">
       <Header />
+      <img className="page-img" src={studioImg} />
       <Nav />
-      <ContentBlock
-        id="présentation"
-        title={data.question}
-        titleIntroText={data.présentation}
-      >
-        <ContentBlock
-          contentImage={img}
-          contentText={data.présentation_perso}
-        />
-      </ContentBlock>
-      <ContentBlock title="Mes activités">
-        <ContentBlock
-          icon={camera}
-          title={tournage.titre}
-          contentText={tournage.content}
-        />
-        <ContentBlock
-          icon={headphones}
-          title={mixage.titre}
-          contentText={mixage.content}
-        />
-        <ContentBlock
-          icon={sound}
-          title={montage.titre}
-          contentText={montage.content}
-        />
-      </ContentBlock>
+      <main>
+        <div className="content-container">
+          <div className="content-block">
+            <h2 className="title">{data.question}</h2>
+            <span className="intro-text">
+              {data.présentation}
+            </span>
+            <ContentBlock
+              id="présentation-perso"
+              contentImage={img}
+              contentText={data.présentation_perso}
+            />
+          </div>
+
+          <div className="content-block">
+            <h2 className="title">Mes activités</h2>
+            <div className="activité-container">
+              <ContentBlock
+                icon={camera}
+                title={tournage.titre}
+                contentText={tournage.content}
+              />
+              <ContentBlock
+                icon={headphones}
+                title={mixage.titre}
+                contentText={mixage.content}
+              />
+              <ContentBlock
+                icon={sound}
+                title={montage.titre}
+                contentText={montage.content}
+              />
+            </div>
+          </div>
+        </div>
+      </main>
+
       <Footer />
     </div>
   );
